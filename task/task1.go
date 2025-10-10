7f76108bad48e704fb280993f993dda627d49075
JinLong Wu <2474323762@qq.com>
2025-10-10 15:28:03 +0800

    feat: 完成任务1

--- task/task1.go
+++ task/task1.go
@@ -1,5 +1,7 @@
 package task
 
+import "sort"
+
 // 136. 只出现一次的数字：给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。可以使用 for 循环遍历数组，结合 if 条件判断和 map 数据结构来解决，例如通过 map 记录每个元素出现的次数，然后再遍历 map 找到出现次数为1的元素。
 var countMap = make(map[int]int)
 
@@ -58,9 +60,95 @@ func IsValid(s string) bool {
 //编写一个函数来查找字符串数组中的最长公共前缀。
 //如果不存在公共前缀，返回空字符串 ""。
 
-//func LongestCommonPrefix(strs []string) string {
-//	prefix := strs[0][0:1]
-//	for _, str := range strs {
-//		if
-//	}
-//}
+func LongestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
+
+//给定一个表示 大整数 的整数数组 digits，其中 digits[i] 是整数的第 i 位数字。这些数字按从左到右，从最高位到最低位排列。这个大整数不包含任何前导 0。
+//将大整数加 1，并返回结果的数字数组。
+
+func plusOne(digits []int) []int {
+	n := len(digits)
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			for j := i + 1; j < n; j++ {
+				digits[j] = 0
+			}
+			return digits
+		}
+	}
+	// digits 中所有的元素均为 9
+
+	digits = make([]int, n+1)
+	digits[0] = 1
+	return digits
+}
+
+//给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+//考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+//更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+//返回 k 。
+
+func removeDuplicates(nums []int) int {
+	n := len(nums)
+	if n <= 1 {
+		return n
+	}
+
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
+// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+func Merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	n := len(intervals)
+	res := [][]int{intervals[0]}
+
+	for i := 1; i < n; i++ {
+		if intervals[i][0] <= res[len(res)-1][1] {
+			if intervals[i][1] > res[len(res)-1][1] {
+				res[len(res)-1] = []int{res[len(res)-1][0], intervals[i][1]}
+			} else {
+				res[len(res)-1] = []int{res[len(res)-1][0], res[len(res)-1][1]}
+			}
+		} else {
+			res = append(res, intervals[i])
+		}
+	}
+	return res
+}
+
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+// 你可以按任意顺序返回答案。
+func twoSum(nums []int, target int) []int {
+	hashTable := make(map[int]int)
+	for i, x := range nums {
+		if j, ok := hashTable[target-x]; ok {
+			return []int{i, j}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
