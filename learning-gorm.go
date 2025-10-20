a57d19dba2a0e7986c4a86836cd88301c4dbe797
Delon-Wu <2474323762@qq.com>
2025-10-20 23:00:07 +0800

    feat: 关联1

--- learning-gorm.go
+++ learning-gorm.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -12,6 +13,8 @@ import (
 // 数据库可选：
 // 1. go get -u gorm.io/driver/sqlite
 // 2. go get -u gorm.io/driver/mysql
+
+// windows 系统打开mysql服务cl(以管理员身份运行cmd)：net start mysql80
 
 // User 带 * 的指针，当为空是默认值是nil
 type User struct {
@@ -25,6 +28,28 @@ type User struct {
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	ignored      string
+
+	CompanyID   uint    // 要和Code的数据类型保持一致
+	Company     Company `gorm:"foreignKey:CompanyID;references: Code"`
+	CreditCard1 CreditCard1
+	Language    []Language `gorm:"many2many:user_languages;"`
+}
+
+type Company struct {
+	gorm.Model
+	Code uint   `gorm:"size:255;unique"`
+	Name string `gorm:"size:255"`
+}
+
+type CreditCard1 struct {
+	gorm.Model
+	Number string `gorm:"size:255"`
+	UserID uint
+}
+
+type Language struct {
+	gorm.Model
+	Name string `gorm:"size:255"`
 }
 
 type Member struct {
@@ -34,7 +59,7 @@ type Member struct {
 }
 
 type Author struct {
-	Name  string `gorm:"size:255"`
+	Name  string `gorm:"size:255;"`
 	Email string `gorm:"size:255"`
 }
 
@@ -44,21 +69,71 @@ type Blog struct {
 	Upvotes int32  `gorm:"default:0"`
 }
 
+func (b *Blog) BeforeCreate(tx *gorm.DB) (err error) {
+	b.Upvotes = 0
+	return nil
+}
+
 type Blog2 struct {
 	ID      uint64 `gorm:"primarykey"`
 	Upvotes int32  `gorm:"default:0"`
 }
 
-func Run(db *gorm.DB) {
+func Run1(db *gorm.DB) {
 	db.AutoMigrate(&User{})
+	db.Create(&User{
+		Name: "John Doe",
+		Age:  18,
+	})
+}
+
+func Run2(db *gorm.DB) {
+	//db.AutoMigrate(&User{})
+	//db.AutoMigrate(&Company{})
+	//db.AutoMigrate(&CreditCard1{})
+	//db.AutoMigrate(&Language{})
+	//
+	//now := time.Now()
+	//
+	//company := Company{Name: "Johnson", Code: 123888}
+	//result := db.Create(&company)
+	//if result.Error != nil {
+	//	fmt.Println(result.Error)
+	//}
+	//fmt.Println("Company ID:", company.ID)
+	//user := User{Name: "Demon Doe", Age: 90, Birthday: &now, CompanyID: 123888}
+	//db.Create(&user)
+
+	//user := User{}
+	//db.Preload("Company").First(&user)
+	//fmt.Printf("user: %+v\n", user)
+
+	//db.First(&user)
+	//fmt.Printf("user: %+v\n", user)
+	//cards := []*CreditCard1{{Number: "800012", UserID: user.ID}, {Number: "800013", UserID: user.ID}}
+	//db.Create(cards)
+
+	//user := User{}
+	//db.Preload("CreditCard1").First(&user)
+	//fmt.Println(user)
+
+	//languages := []*Language{{Name: "English"}, {Name: "Chinese"}, {Name: "French"}}
+	//db.Create(languages)
+	var languages []Language
+	db.Find(&languages)
+	fmt.Println("All languages: ", languages)
+	user := User{Name: "Old John", Age: 199, Language: languages, CompanyID: 123888}
+	db.Create(&user)
 }
 
 func main() {
 	//db, err := gorm.Open(mysql.Open("root:st123456@tcp（这是用户名，密码）(127.0.0.1:3306)/gorm(这是数据库名)?charset=utf8（这是编码格式）&parseTime=True（将golang的time转成数据库支持的）&loc=Local"), &gorm.Config{})
-	db, err := gorm.Open(mysql.Open("root:st123456@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
+		fmt.Println(err)
 		panic("failed to connect database")
 	}
 
-	Run(db)
+	//Run1(db)
+	Run2(db)
 }
