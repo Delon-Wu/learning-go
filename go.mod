fbcca47c2d6a448187fa060353f31c5755726d84
JinLong Wu <2474323762@qq.com>
2025-10-22 17:29:23 +0800

    feat: 学习了关联表的创建更新

--- go.mod
+++ go.mod
@@ -3,11 +3,22 @@ module github.com/Delon-Wu/learning-go.git
 go 1.25.1
 
 require (
+	gorm.io/driver/mysql v1.6.0
+	gorm.io/gorm v1.31.0
+)
+
+//# 下载所有依赖
+//go mod download
+//
+//# 下载依赖并验证校验和
+//go mod verify
+//
+//# 下载依赖并整理 go.mod 文件
+//go mod tidy
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/text v0.30.0 // indirect
-	gorm.io/driver/mysql v1.6.0 // indirect
-	gorm.io/gorm v1.31.0 // indirect
 )
--- learning-gorm.go
+++ learning-gorm.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -15,16 +16,39 @@ import (
 
 // User 带 * 的指针，当为空是默认值是nil
 type User struct {
-	ID           uint    `gorm:"primarykey"`
-	Name         string  `gorm:"size:255"`
-	Email        *string `gorm:"size:255"`
-	Age          uint8
-	Birthday     *time.Time
-	MemberNumber sql.NullString
-	ActivatedAt  sql.NullTime
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	ignored      string
+	ID                uint   `gorm:"primarykey"`
+	Name              string `gorm:"size:255"`
+	Age               uint8
+	Birthday          *time.Time
+	MemberNumber      sql.NullString
+	ActivatedAt       sql.NullTime
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	ignored           string
+	BillingAddressID  uint
+	BillingAddress    Address `gorm:"foreignkey:BillingAddressID"`
+	ShippingAddressID uint
+	ShippingAddress   Address    `gorm:"foreignkey:ShippingAddressID"`
+	Languages         []Language `gorm:"many2many:user_languages;"`
+	Emails            []Email
+}
+
+type Address struct {
+	gorm.Model
+	Address1 string
+	UserID   uint
+}
+
+type Language struct {
+	gorm.Model
+	Name   string
+	UserID uint
+}
+
+type Email struct {
+	gorm.Model
+	Email  string
+	UserID uint
 }
 
 type Member struct {
@@ -49,16 +73,98 @@ type Blog2 struct {
 	Upvotes int32  `gorm:"default:0"`
 }
 
-func Run(db *gorm.DB) {
+func Run1(db *gorm.DB) {
 	db.AutoMigrate(&User{})
+}
+
+func Run2(db *gorm.DB) {
+	// CURD
+	user := User{
+		Name: "Delon",
+		Age:  18,
+	}
+	result := db.Create(&user)
+	if result.Error != nil {
+		fmt.Println("Error creating user", result.Error)
+	}
+	fmt.Println("Single user created", user)
+
+	users := []*User{
+		{Name: "Tom", Age: 9},
+		{Name: "Jack", Age: 17},
+		{Name: "Janice", Age: 59},
+	}
+	result = db.Create(&users)
+	if result.Error != nil {
+		fmt.Println("Error creating users", result.Error)
+	}
+	fmt.Println("Users created", result)
+}
+
+func Run3(db *gorm.DB) {
+	//db.AutoMigrate(&User{})
+	//db.AutoMigrate(&Address{})
+	//db.AutoMigrate(&Language{})
+	//db.AutoMigrate(&Email{})
+	//user := User{
+	//	Name:            "Chandler",
+	//	Age:             25,
+	//	BillingAddress:  Address{Address1: "Billing Address1 - Address 1"},
+	//	ShippingAddress: Address{Address1: "Shipping Address1 - Address 1"},
+	//	Emails: []Email{
+	//		{Email: "666666@qq.com"},
+	//		{Email: "888@qq.com"},
+	//	},
+	//	Languages: []Language{
+	//		{Name: "English"},
+	//		{Name: "Chinese"},
+	//		{Name: "French"},
+	//	},
+	//}
+	////创建时自动保存关联
+	//db.Create(&user)
+	//db.Save(&user)
+
+	//user := User{}
+	//db.Find(&user, 1)
+	//user.BillingAddress = Address{Address1: "new billing address 2"} // 新增记录
+	//user.BillingAddress.Address1 = "new billing address 2" // 更新记录
+	//db.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&user)
+
+	//db.Preload("BillingAddress").Find(&user, 1)
+	//fmt.Println("User: ", user)
+
+	// 完善User的定义（已包含所有关联字段）
+
+	// 删除id为2和3的用户及其所有关联信息
+	//var users []User
+	//db.Find(&users, []int{2, 3})
+	//for _, u := range users {
+	//	db.Select(clause.Associations).Delete(&u)
+	//}
+
+	var user User
+	////error := db.Model(&user).Association("Languages").Error
+	//var languages []Language
+	//err := db.Model(&user).Association("Languages").Find(&languages)
+	//if err != nil {
+	//	return
+	//}
+	//fmt.Println("Languages:\n", languages)
+
+	ids := []uint{0, 1, 2}
+	db.Model(&user).Where("ID IN ?", ids).Association("Languages").Clear()
 }
 
 func main() {
 	//db, err := gorm.Open(mysql.Open("root:st123456@tcp（这是用户名，密码）(127.0.0.1:3306)/gorm(这是数据库名)?charset=utf8（这是编码格式）&parseTime=True（将golang的time转成数据库支持的）&loc=Local"), &gorm.Config{})
-	db, err := gorm.Open(mysql.Open("root:st123456@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
+		fmt.Println(err)
 		panic("failed to connect database")
 	}
 
-	Run(db)
+	//Run1(db)
+	//Run2(db)
+	Run3(db)
 }
